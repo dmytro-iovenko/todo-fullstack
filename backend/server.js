8faa460422f6652f12945564dbd4013d459e2ede
Dmytro Iovenko <iovenko@gmail.com>
2024-10-03 14:34:58 -0400

    added a get todos route and hit a request to it from the frontend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import cors from "cors";
 import "dotenv/config";
 // bring in the function that will make the connetcion to the database
 import connectDB from "./config.js";
+// bring in our todo model to interact with the client
+import Todo from "./models/todoModel.js";
 
 // create our express app
 const app = express();
@@ -19,6 +21,20 @@ const PORT = 8080;
 // at least one basic route for testing purposes
 app.get("/test", (req, res) => {
   res.json("Hello (from Server)!");
+});
+
+// a route that gets all todos and sends it to client
+app.get("/todos", async (req, res) => {
+  try {
+    // use find methid on the model to retrieve all documents from the todos collection
+    const todos = await Todo.find();
+    console.log("GET /todos")
+    // send those documents to the client
+    res.status(200).json(todos);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
 });
 
 // setup our server to listen on a specific port
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   useEffect(() => {
     let isMounted = true;
     (async () => {
-      const response = isMounted && await fetch("http://localhost:8080/test");
-      const data = isMounted && await response.json();
+      const response = isMounted && (await fetch("http://localhost:8080/todos"));
+      const data = isMounted && (await response.json());
       isMounted && console.log(data);
+      isMounted && setTodos(data);
     })();
     return () => {
       isMounted = false;
