f2332e383fed3e6413570e69e8e08f3ef906d0df
Dmytro Iovenko <iovenko@gmail.com>
2024-10-03 16:24:07 -0400

    added an env variable for our base url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const BASE_URL = "http://localhost:8080"
+export const BASE_URL = import.meta.env.VITE_BASE_URL
 
 function App() {
   const [todos, setTodos] = useState([]);
