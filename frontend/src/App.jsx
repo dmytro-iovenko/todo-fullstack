f36beaed37670ecb77404d6733473ae70b3d2361
Dmytro Iovenko <iovenko@gmail.com>
2024-10-03 12:08:14 -0400

    added a useEffect to the frontend to attempt a request to the backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+  useEffect(() => {
+    async function test() {
+      const response = await fetch("http://localhost:8080/test");
+      const data = await response.json();
+      console.log(data);
+    }
+    test();
+  }, []);
+  return <></>;
 }
 
-export default App
+export default App;
