41075bd54363e1d267f9dd7f4e4b5e69f6f15dde
Dmytro Iovenko <iovenko@gmail.com>
2024-10-03 16:16:33 -0400

    added the logic in the frontend for deleting a todo by id

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -48,14 +48,32 @@ function App() {
     console.log(newTodo);
   }
 
-  console.log(input);
+  // the id is the _id of the todo document we want to delete
+  async function handleDelete(id) {
+    // make the request with the document id in the path (at the end)
+    const response = await fetch(`http://localhost:8080/todos/${id}`, {
+      method: "DELETE",
+    });
+
+    // format the deleted todo
+    const deletedTodo = await response.json();
+
+    // make a copy of the state but also remove the document with the mathcing id
+    const updatedTodos = todos.filter((todo) => todo._id !== deletedTodo._id);
+
+    // update the state with a new array
+    setTodos(updatedTodos);
+  }
 
   return (
     <>
       <h1>Todo:</h1>
       <ul>
         {todos.map((todo) => (
-          <li key={todo._id}>{todo.text}</li>
+          <li key={todo._id}>
+            {todo.text}
+            <button onClick={() => handleDelete(todo._id)}>X</button>
+          </li>
         ))}
       </ul>
       <form onSubmit={handleSubmit}>
